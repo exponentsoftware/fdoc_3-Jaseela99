b7a5f1803ae49b3e7a25d7a5aeef3563f7189607
Jaseela99 <jaseelachittur@gmail.com>
2022-05-19 00:06:32 +0530

    completed 1

--- 1.js
+++ 1.js
@@ -10,3 +10,15 @@ const [,,jsScore,reactScore] = scores
 console.log(jsScore,reactScore)
 
 
+//1.b 
+const students = [
+    ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
+    ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
+]
+const convertArrayToObject =(arr)=>{
+    const stuObj = arr.map(stu=>{Object.fromEntries(stu)})
+    return stuObj
+
+}
+
+console.log(convertArrayToObject(students))
