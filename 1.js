047e6422a108db8e6b0dc2ccad6abb3807736898
Jaseela99 <jaseelachittur@gmail.com>
2022-05-22 00:22:56 +0530

    1 completed

--- 1.js
+++ 1.js
@@ -1,5 +1,5 @@
 //1.a
-/*  const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
+  const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
  //array destructuring
  //spread operator is used to clone the array 
  const [name,[...skills],[...scores]] = student
@@ -8,7 +8,7 @@
  //to skip an element while destructuring then we use an extra comma(,)
 const [,,jsScore,reactScore] = scores
 console.log(jsScore,reactScore)
- */
+ 
 //1.b
 
 const students = [
@@ -17,19 +17,20 @@ const students = [
 ]
 
 const convertArrayToObject = (arr) => {
+    //array and object is defined
     let array = []
     var obj = {}
+    //loop through the array
     for (let i = 0; i < arr.length; i++) {
         obj = {
-            name: arr[i][0],
-            skills: arr[i][1],
-            scores: arr[i][2]
+            name: arr[i][0], //j=1 is name
+            skills: arr[i][1],//j=2 is skills
+            scores: arr[i][2]//j=3 is scores
         }
-        array.push(obj)
-
-        
+        //push the object into the array
+        array.push(obj)    
     }
-    
+    //return the array
     return array
 }
 
